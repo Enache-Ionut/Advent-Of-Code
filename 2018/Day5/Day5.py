fad0749d38dc9a684590f12934cdeaf5691b6fe2
Enache Ionut <enachestoianionut@gmail.com>
2018-12-13 11:47:31 +0200

    solve the day 5 challange

--- 2018/Day5/Day5.py
+++ 2018/Day5/Day5.py
@@ -1,17 +1,26 @@
-#part 1
+def part1(inputStr):
+    tryAgain = True
+    while tryAgain:
+        tryAgain = False
+        first, second = 1, 0
+        while first < len(inputStr):
+            if abs(ord(inputStr[first]) - ord(inputStr[second])) == 32:
+                inputStr = inputStr[:second] + inputStr[first + 1:]
+                tryAgain = True
+                break
+            else:
+                first += 1
+                second += 1
+    return len(inputStr)
 
-input = open('inputDay5', 'r').read()
-tryAgain = True
-while tryAgain:
-    tryAgain = False
-    first, second = 1, 0
-    while first < len(input):
-        if abs(ord(input[first]) - ord(input[second])) == 32:
-            input = input[:second] + input[first+1:]
-            tryAgain = True
-            break;
-        else:
-            first = first + 1
-            second = second + 1
+def part2(input):
+    minLength = []
+    uniqueLetters = set(input.lower())
+    for ch in uniqueLetters:
+        newLine = input.replace(ch, '')
+        newLine = newLine.replace(chr(ord(ch)-32), '')
+        minLength.append(part1(newLine))
+    return min(minLength)
 
-print(len(input))
+print(part1(open('inputDay5Part1', 'r').read()))
+print(part2(open('inputDay5Part2', 'r').read()))
